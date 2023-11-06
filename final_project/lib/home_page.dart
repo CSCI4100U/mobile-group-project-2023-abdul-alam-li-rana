de3c03e31a6ecdabc90728ff8b5209d98fc30ae6
Almas Alam <Almas.alam@ontariotechu.net>
2023-11-05 23:20:44 -0500

    Home_page.dart

--- final_project/lib/home_page.dart
+++ final_project/lib/home_page.dart
@@ -5,12 +5,11 @@ import 'help_page.dart';
 import 'service_page.dart';
 import 'sidebar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double buttonWidth = MediaQuery.of(context).size.width - 75;
-    double buttonHeight = 175.0;
-
+    double buttonSize = 175.0;
     TextStyle buttonTextStyle = TextStyle(
       fontSize: 30.0,
       fontWeight: FontWeight.bold,
@@ -20,82 +19,127 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(Icons.leave_bags_at_home))
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.leave_bags_at_home),
+          )
         ],
-        backgroundColor: const Color.fromARGB(160, 255, 255, 255),
+        backgroundColor: Colors.lightBlue,
         title: Text('Vehicle Management App'),
         centerTitle: true,
-        automaticallyImplyLeading: false, // Prevent back arrow
+        automaticallyImplyLeading: false,
       ),
-      backgroundColor: Colors.grey,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              child: SizedBox(
-                width: buttonWidth,
-                height: buttonHeight,
-                child: ElevatedButton(
-                  onPressed: () {
+      backgroundColor: Colors.transparent, // Make the background transparent
+      body: Stack(
+        children: [
+          _buildGradientBackground(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildShadedButton(
+                  buttonSize,
+                  Colors.teal,
+                  Icons.directions_car,
+                  'Vehicles',
+                  buttonTextStyle,
+                      () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (context) => VehicleHomePage()));
                   },
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('Vehicles', style: buttonTextStyle),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              child: SizedBox(
-                width: buttonWidth,
-                height: buttonHeight,
-                child: ElevatedButton(
-                  onPressed: () {
+                _buildShadedButton(
+                  buttonSize,
+                  Colors.indigo,
+                  Icons.airplanemode_active,
+                  'Trip',
+                  buttonTextStyle,
+                      () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (context) => TripPage()));
                   },
-                  style: ElevatedButton.styleFrom(primary: Colors.blue),
-                  child: Text('Trip', style: buttonTextStyle),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              child: SizedBox(
-                width: buttonWidth,
-                height: buttonHeight,
-                child: ElevatedButton(
-                  onPressed: () {
+                _buildShadedButton(
+                  buttonSize,
+                  Colors.pink,
+                  Icons.build,
+                  'Service',
+                  buttonTextStyle,
+                      () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (context) => ServicePage()));
                   },
-                  style: ElevatedButton.styleFrom(primary: Colors.red),
-                  child: Text('Service', style: buttonTextStyle),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: SizedBox(
-                  width: buttonWidth,
-                  height: buttonHeight,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HelpPage()));
-                    },
-                    style: ElevatedButton.styleFrom(primary: Colors.orange),
-                    child: Text('Help', style: buttonTextStyle),
-                  ),
+                _buildShadedButton(
+                  buttonSize,
+                  Colors.amber,
+                  Icons.help,
+                  'Help',
+                  buttonTextStyle,
+                      () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => HelpPage()));
+                  },
                 ),
-              ),
+              ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGradientBackground() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF3366FF), // Adjust gradient colors as per your preference
+            Color(0xFF6633FF),
           ],
         ),
       ),
     );
   }
+
+  Widget _buildShadedButton(
+      double buttonSize,
+      Color color,
+      IconData iconData,
+      String label,
+      TextStyle textStyle,
+      void Function() onPressed,
+      ) {
+    return Container(
+      width: buttonSize,
+      height: buttonSize,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+        boxShadow: [
+          BoxShadow(
+            color: color.withOpacity(0.6),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onPressed,
+          customBorder: CircleBorder(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(iconData, size: 80, color: Colors.white),
+              SizedBox(height: 10),
+              Text(label, style: textStyle.copyWith(color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+
