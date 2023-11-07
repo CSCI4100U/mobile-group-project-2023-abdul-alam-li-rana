0fa12809df4cec81c3f25c707044b2fcd06deac2
Ju5t1nLi <justin.li3@ontariotechu.net>
2023-11-07 12:07:57 -0500

    verification freeze fixed

--- final_project/lib/verification_screen.dart
+++ final_project/lib/verification_screen.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
 import 'login.dart';
+import 'home_page.dart';
+
 class EmailVerificationScreen extends StatefulWidget {
   const EmailVerificationScreen({Key? key}) : super(key: key);
 
@@ -13,14 +15,16 @@ class EmailVerificationScreen extends StatefulWidget {
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   bool isEmailVerified = false;
-  Timer? timer;
+  bool emailSent = false;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    FirebaseAuth.instance.currentUser?.sendEmailVerification();
-    timer =
-        Timer.periodic(const Duration(seconds: 3), (_) => checkEmailVerified());
+    checkEmailVerified(); // Check email verification immediately when the screen loads.
+    if (emailSent == false){
+      FirebaseAuth.instance.currentUser?.sendEmailVerification();
+      emailSent = true;
+    }
   }
 
   checkEmailVerified() async {
@@ -31,88 +35,92 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
     });
 
     if (isEmailVerified) {
-      // TODO: implement your code after email verification
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text("Email Successfully Verified")));
-          Navigator.pop(context);
-
-      timer?.cancel();
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Email Successfully Verified")));
+      Navigator.pop(context);
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
     }
   }
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    timer?.cancel();
-    super.dispose();
-  }
-@override
-Widget build(BuildContext context) {
-  FirebaseAuth auth = FirebaseAuth.instance;
-  return SafeArea(
-    child: Scaffold(
-      appBar: AppBar(
-               actions: [
-          IconButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(Icons.logout),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 35),
-            const SizedBox(height: 30),
-            const Center(
-              child: Text(
-                'Check your \n Email',
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Center(
-                child: Text(
-                  'We have sent you an Email on ${auth.currentUser?.email}',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            const Center(child: CircularProgressIndicator()),
-            const SizedBox(height: 8),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32.0),
-              child: Center(
-                child: Text(
-                  'Verifying email....',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            const SizedBox(height: 57),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: ElevatedButton(
-                child: const Text('Resend'),
-                onPressed: () {
-                  try {
-                    FirebaseAuth.instance.currentUser?.sendEmailVerification();
-                  } catch (e) {
-                    debugPrint('$e');
-                  }
-                },
-              ),
-            ),
+  Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(Icons.logout),
+            )
           ],
         ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 35),
+              const SizedBox(height: 30),
+              const Center(
+                child: Text(
+                  'Check your \n Email',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Center(
+                  child: Text(
+                    'We have sent you an Email on ${auth.currentUser?.email}',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              isEmailVerified
+                  ? const Center(child: Text('Email Successfully Verified'))
+                  : const Center(child: CircularProgressIndicator()),
+              const SizedBox(height: 8),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0),
+                child: Center(
+                  child: Text(
+                    'Verifying email....',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 57),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: ElevatedButton(
+                  child: const Text('Resend'),
+                  onPressed: () {
+                    try {
+                      FirebaseAuth.instance.currentUser?.sendEmailVerification();
+                    } catch (e) {
+                      debugPrint('$e');
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(height: 57), // Add spacing between Resend and Check Verification buttons
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: ElevatedButton(
+                  child: const Text('Check Verification'),
+                  onPressed: () {
+                    checkEmailVerified(); // Check email verification when the button is pressed.
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
