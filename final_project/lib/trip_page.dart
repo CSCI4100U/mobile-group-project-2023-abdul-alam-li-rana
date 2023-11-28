3fafc5046d6a287238942b90ac6fe986caf35870
Ju5t1nLi <justin.li3@ontariotechu.net>
2023-11-27 19:52:01 -0500

    map and route navigation functionality added

--- final_project/lib/trip_page.dart
+++ final_project/lib/trip_page.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'package:mapbox_gl/mapbox_gl.dart';
 import 'address_complete.dart';
 import 'network_utility.dart';
 import 'gas.dart';
+import 'package:mapbox_polyline_points/mapbox_polyline_points.dart';
+
 class TripPage extends StatefulWidget {
   @override
   _TripPageState createState() => _TripPageState();
@@ -19,44 +25,143 @@ class _TripPageState extends State<TripPage> {
   TextEditingController sourceController = TextEditingController();
   TextEditingController destinationController = TextEditingController();
 
-  Future<void> placeAutocomplete(String query, bool isSource) async {
-  Uri uri = Uri.https(
-    "maps.googleapis.com",
-    'maps/api/place/autocomplete/json',
-    {
-      "input": query,
-      "key": 'AIzaSyCK3IDVz6IGqf09tf0dUHf1r_7Ig_tq4N0',
-    },
-  );
-  String? response = await NetworkUtility.fetchUrl(uri);
-
-  if (response != null) {
-    PlaceAutocompleteResponse result =
-        PlaceAutocompleteResponse.parseAutocompleteResult(response);
-    if (result.predictions != null) {
-      setState(() {
-        if (isSource) {
-          sourcePredictions = result.predictions!;
-        } else {
-          destinationPredictions = result.predictions!;
-        }
-      });
-    }
-  }
-}
-
-  void updateSearchBar(String? selectedLocation, TextEditingController controller) {
-  controller.text = selectedLocation ?? '';
-  setState(() {
-    sourcePredictions = [];
-    destinationPredictions = [];
-  });
-}
+  MapboxMapController? _controller;
+  String? routeGeometry;
+  LatLng? _userLocation;
+  bool isRoutePlotted = false;
+  List<PointLatLng>? routePoints;
 
   @override
   void initState() {
     super.initState();
     _getCurrentLocation();
+  }
+
+  Future<void> fetchRouteInformation() async {
+    String mapboxApiKey =
+        "sk.eyJ1IjoianVzdGZhbCIsImEiOiJjbHBoMnFzOGYwM2o5MmlxeGM1MW5wamZoIn0.RFlNRhyj0xccr7MPoULncg";
+
+    clearPolyline();
+    String sourceAddress = sourceController.text;
+    String destinationAddress = destinationController.text;
+
+    if (sourceAddress.isEmpty || destinationAddress.isEmpty) {
+      // Handle the case where either source or destination address is empty
+      return;
+    }
+
+    try {
+      List<Location> sourceLocations = await locationFromAddress(sourceAddress);
+      List<Location> destinationLocations =
+          await locationFromAddress(destinationAddress);
+
+      print('Source Locations: $sourceLocations');
+      print('Destination Locations: $destinationLocations');
+
+      if (sourceLocations.isNotEmpty && destinationLocations.isNotEmpty) {
+        LatLng sourceLatLng =
+            LatLng(sourceLocations[0].latitude, sourceLocations[0].longitude);
+        LatLng destinationLatLng = LatLng(destinationLocations[0].latitude,
+            destinationLocations[0].longitude);
+
+        print('Source LatLng: $sourceLatLng');
+        print('Destination LatLng: $destinationLatLng');
+
+        MapboxpolylinePoints mapboxPolylinePoints = MapboxpolylinePoints();
+        MapboxPolylineResult result =
+            await mapboxPolylinePoints.getRouteBetweenCoordinates(
+          mapboxApiKey,
+          PointLatLng(
+              latitude: sourceLatLng.latitude,
+              longitude: sourceLatLng.longitude),
+          PointLatLng(
+              latitude: destinationLatLng.latitude,
+              longitude: destinationLatLng.longitude),
+          TravelType.driving, // Specify your desired travel mode
+        );
+
+        if (result.points.isNotEmpty) {
+          // The route points are available in result.points
+
+          routePoints = result.points[0];
+
+          // Now you can use the routePoints to draw polylines on the map or perform other tasks.
+          drawPolylineOnMap();
+          print('Route Points: $routePoints');
+        } else {
+          print('No route points available');
+        }
+      } else {
+        print('Error geocoding addresses');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void drawPolylineOnMap() {
+    // Check if the MapboxMapController is initialized
+    if (_controller != null) {
+      // Clear existing polylines on the map
+      _controller!.clearLines();
+
+      // Create a list of LatLng from PointLatLng
+      List<LatLng> polylineLatLngs = routePoints!
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+
+      // Draw polyline on the map
+      _controller!.addLine(
+        LineOptions(
+          geometry: polylineLatLngs,
+          lineColor: '#FF0000', // Adjust the color as needed
+          lineWidth: 3.0, // Adjust the width as needed
+        ),
+      );
+    }
+  }
+
+  void clearPolyline() {
+  // Check if the MapboxMapController is initialized
+  if (_controller != null) {
+    // Clear existing polylines on the map
+    _controller!.clearLines();
+  }
+}
+
+  Future<void> placeAutocomplete(String query, bool isSource) async {
+    Uri uri = Uri.https(
+      "maps.googleapis.com",
+      'maps/api/place/autocomplete/json',
+      {
+        "input": query,
+        "key": 'AIzaSyCK3IDVz6IGqf09tf0dUHf1r_7Ig_tq4N0',
+      },
+    );
+    String? response = await NetworkUtility.fetchUrl(uri);
+
+    if (response != null) {
+      PlaceAutocompleteResponse result =
+          PlaceAutocompleteResponse.parseAutocompleteResult(response);
+      if (result.predictions != null) {
+        setState(() {
+          if (isSource) {
+            sourcePredictions = result.predictions!;
+          } else {
+            destinationPredictions = result.predictions!;
+          }
+        });
+      }
+    }
+  }
+
+  void updateSearchBar(
+      String? selectedLocation, TextEditingController controller) {
+    controller.text = selectedLocation ?? '';
+    setState(() {
+      sourcePredictions = [];
+      destinationPredictions = [];
+    });
   }
 
   Future<void> _getCurrentLocation() async {
@@ -65,12 +170,10 @@ class _TripPageState extends State<TripPage> {
       if (permission == LocationPermission.denied) {
         permission = await Geolocator.requestPermission();
         if (permission == LocationPermission.denied) {
-          
           return;
         }
       }
       if (permission == LocationPermission.deniedForever) {
-
         return;
       }
 
@@ -86,6 +189,7 @@ class _TripPageState extends State<TripPage> {
       String country = placemarks.first.country ?? 'Unknown';
 
       setState(() {
+        _userLocation = LatLng(position.latitude, position.longitude);
         _country = country;
       });
     } catch (e) {
@@ -95,7 +199,7 @@ class _TripPageState extends State<TripPage> {
 
   void _checkGasPrice() {
     String gasPrice = "2.50";
-    getGasPrice(); 
+    getGasPrice();
     _displayGasPrice(gasPrice);
   }
 
@@ -137,7 +241,8 @@ class _TripPageState extends State<TripPage> {
                   child: TextField(
                     controller: sourceController,
                     onChanged: (value) {
-                      placeAutocomplete(value,true);
+                      placeAutocomplete(value, true);
+                      clearPolyline();
                     },
                     decoration: InputDecoration(
                       labelText: 'From',
@@ -157,8 +262,7 @@ class _TripPageState extends State<TripPage> {
                       return ListTile(
                         title: Text(sourcePredictions[index].description ?? ''),
                         onTap: () {
-                          updateSearchBar(
-                              sourcePredictions[index].description,
+                          updateSearchBar(sourcePredictions[index].description,
                               sourceController);
                         },
                       );
@@ -173,7 +277,8 @@ class _TripPageState extends State<TripPage> {
                   child: TextField(
                     controller: destinationController,
                     onChanged: (value) {
-                      placeAutocomplete(value,false);
+                      placeAutocomplete(value, false);
+                      clearPolyline();
                     },
                     decoration: InputDecoration(
                       labelText: 'To',
@@ -191,7 +296,8 @@ class _TripPageState extends State<TripPage> {
                     itemCount: destinationPredictions.length,
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text(destinationPredictions[index].description ?? ''),
+                        title: Text(
+                            destinationPredictions[index].description ?? ''),
                         onTap: () {
                           updateSearchBar(
                               destinationPredictions[index].description,
@@ -203,8 +309,6 @@ class _TripPageState extends State<TripPage> {
                 ),
               ),
             SizedBox(height: 20),
-
-
             GestureDetector(
               onTap: _checkGasPrice,
               child: Container(
@@ -226,13 +330,41 @@ class _TripPageState extends State<TripPage> {
                 ),
               ),
             ),
+            // Button to plot GPS route
+            ElevatedButton(
+              onPressed: () {
+                // Check if the route is already plotted
+                if (!isRoutePlotted) {
+                  // Call the method to fetch and plot the route
+                  fetchRouteInformation();
+                  isRoutePlotted = true; // Update the flag
+                }
+              },
+              child: Text('Plot Route'),
+            ),
+            Container(
+              height: 300, // Adjust the height as needed
+              child: MapboxMap(
+                accessToken:
+                    "sk.eyJ1IjoianVzdGZhbCIsImEiOiJjbHBoMnFzOGYwM2o5MmlxeGM1MW5wamZoIn0.RFlNRhyj0xccr7MPoULncg",
+                initialCameraPosition: CameraPosition(
+                  target:
+                      LatLng(_userLocation!.latitude, _userLocation!.longitude),
+                      //LatLng(43.9515, -78.8567),
+                  zoom: 12.0,
+                ),
+                onMapCreated: (MapboxMapController controller) {
+                  _controller = controller;
+                  // Ensure that the route is plotted when the map is created
+                  if (isRoutePlotted) {
+                    fetchRouteInformation();
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
