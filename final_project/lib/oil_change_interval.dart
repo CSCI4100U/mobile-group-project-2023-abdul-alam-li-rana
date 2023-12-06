b1b3ca8a5502e1159558913443a7e832a07886ac
Almas Alam <Almas.alam@ontariotechu.net>
2023-12-05 22:03:17 -0500

    Implemented a page within service tab, in order to calculate oil service need more help

---
+++ final_project/lib/oil_change_interval.dart
@@ -0,0 +1,12 @@
+class OilChangeInterval {
+  final String vehicleId;
+  final int lastOilChangeMileage;
+  final String recommendedInterval;
+
+
+  OilChangeInterval({
+    required this.vehicleId,
+    required this.lastOilChangeMileage,
+    required this.recommendedInterval,
+  });
+}
--- final_project/lib/service_page.dart
+++ final_project/lib/service_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'sidebar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'oil_change_interval_page.dart'; // Import the OilChangeIntervalPage
 
 class ServicePage extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -30,8 +31,13 @@ class ServicePage extends StatelessWidget {
               StaggeredTile.fit(index.isEven ? 1 : 2),
           mainAxisSpacing: 16.0,
           crossAxisSpacing: 16.0,
-          itemBuilder: (BuildContext context, int index) =>
-              _buildServiceCard(context, services[index]),
+          itemBuilder: (BuildContext context, int index) {
+            if (services[index].title == 'Oil Change Interval') {
+              return _buildOilChangeButton(context, services[index]);
+            } else {
+              return _buildServiceCard(context, services[index]);
+            }
+          },
         ),
       ),
       drawer: SideMenu(parentContext: context),
@@ -59,7 +65,68 @@ class ServicePage extends StatelessWidget {
                 service.icon,
                 height: 80.0,
                 width: 80.0,
-                color: Theme.of(context).primaryColor,
+                color: Theme
+                    .of(context)
+                    .primaryColor,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                service.title,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                service.description,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOilChangeButton(BuildContext context, Service service) {
+    // Assuming you have a vehicleId available, replace 'your_vehicle_id' with the actual id
+    String vehicleId = 'your_vehicle_id';
+
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: InkWell(
+        onTap: () {
+          // Navigate to the OilChangeIntervalPage when tapped
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OilChangeIntervalPage(vehicleId: vehicleId),
+            ),
+          );
+        },
+        borderRadius: BorderRadius.circular(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                service.icon,
+                height: 80.0,
+                width: 80.0,
+                color: Theme
+                    .of(context)
+                    .primaryColor,
               ),
               SizedBox(height: 16.0),
               Text(
@@ -87,7 +154,7 @@ class ServicePage extends StatelessWidget {
   }
 }
 
-class Service {
+  class Service {
   final String icon;
   final String title;
   final String description;
@@ -120,5 +187,11 @@ final List<Service> services = [
     icon: 'assets/icons/help.svg',
     title: '24/7 Support',
     description: 'Always here to assist you with our 24/7 customer support.',
+  ),
+
+  Service(
+    icon: 'assets/icons/oil-change.svg',
+    title: 'Oil Change Interval',
+    description: 'Track and schedule your oil change intervals.',
   ),
 ];
